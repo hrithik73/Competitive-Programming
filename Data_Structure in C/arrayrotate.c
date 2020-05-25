afe2a077ea13915b18514dc066acca31bdae57e8
hrithik73 <hrithiksinghvps_cse18@its.edu.in>
2020-05-25 22:49:25 +0530

    Changes

--- Data_Structure in C/arrayrotate.c
+++ Data_Structure in C/arrayrotate.c
@@ -1,40 +1,40 @@
 //jiggling Algorithm for Array Rotation
 #include<stdio.h>
-int gcd(int m,int n){
-	if(n==0)
-	return m;
-	else 
-	return gcd(m,n%m);
+int gcd(int m, int n) {
+  if (n == 0)
+    return m;
+  else
+    return gcd(m, n % m);
 }
 
-void Arrayrotate(int A[],int k,int n){
-int i,j,temp=0,d=0,GCD=gcd(n,k);
-for(i=0;i<GCD;i++){
-  	temp=A[i];
-  	j=i;
-  	while(1){
-  		d=(j+k)%n;
-  		if(d==i)
-  			break;
-  		A[j]=A[d];
-  	}
-  	A[j]=temp;
-  } 
+void Arrayrotate(int A[], int k, int n) {
+  int i, j, temp = 0, d = 0, GCD = gcd(n, k);
+  for (i = 0; i < GCD; i++) {
+    temp = A[i];
+    j = i;
+    while (1) {
+      d = (j + k) % n;
+      if (d == i)
+        break;
+      A[j] = A[d];
+    }
+    A[j] = temp;
+  }
 
 }
 
 
 int main()
 {
-	int A[100],n,k;
-	printf("Enter The No. of Elements In Array\n");
-	scanf("%d",&n);
-	printf("Enter the Elements Of Array\n");
-	for(int i=0;i<n;i++)
-	scanf("%d",&A[i]);
-    printf("Enter The k\n");
-    scanf("%d",&k);
-    Arrayrotate(A,k,n);
-    for(int i=0;i<n;i++)
-    	printf("%d",A[i]);
+  int A[100], n, k;
+  printf("Enter The No. of Elements In Array\n");
+  scanf("%d", &n);
+  printf("Enter the Elements Of Array\n");
+  for (int i = 0; i < n; i++)
+    scanf("%d", &A[i]);
+  printf("Enter The k\n");
+  scanf("%d", &k);
+  Arrayrotate(A, k, n);
+  for (int i = 0; i < n; i++)
+    printf("%d", A[i]);
 }
